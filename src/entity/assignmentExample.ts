c66b7dba26160efc2f95c410e9c68550209a81d7
jamesliudotcc <james@jamesliu.cc>
2019-02-07 10:00:21 -0800

    instructor can grade assignment

--- src/entity/assignmentExample.ts
+++ src/entity/assignmentExample.ts
@@ -1,9 +1,9 @@
-// tslint:disable:no-var-requires
-const en = require('nanoid-good/locale/en');
-const nanoid = require('nanoid-good')(en);
-// tslint:enable:no-var-requires
-
-import { createConnection, getMongoManager, getRepository } from 'typeorm';
+import {
+  createConnection,
+  getMongoManager,
+  getMongoRepository,
+  getRepository,
+} from 'typeorm';
 
 import { Assignment } from './Assignment';
 import { Cohort } from './Cohort';
@@ -23,69 +23,69 @@ createConnection({
 }).then(async connection => {
   try {
     const userRepository = getRepository(User);
+    const deliverableRepository = getMongoRepository(Deliverable);
     const assignmentRepository = getRepository(Assignment);
     const cohortRepository = getRepository(Cohort);
     const manager = getMongoManager();
 
-    // Uncomment to run functions to populate mock data:
-
-    // await createAssignment(userRepository, assignmentRepository, manager);
-    // Uncomment to run functions to populate mock data:
-
+    // // Uncomment to run functions to populate mock data:
     // await createAssignment(userRepository, assignmentRepository, manager);
 
-    // Assignment to each member of cohort
+    // // Assignment to each member of cohort
+    // await assignmentToDeliverables();
 
-    await assignmentToDeliverables();
+    // // Student hands in deliverable with URL:
+    // await handInAssignment(userRepository, deliverableRepository, manager);
 
-    // Student hands in deliverable with URL:
-    // Student pulls a particular deliverable from the list of deliverables
-
-    // // It gets marked turned in.
-
-    // Instructor can find all turned in deliverables
-
-    // Instructor can find all turned in deliverables
-
-    // const users = await userRepository.find();
-    // const students = users
-    //   .filter(s => s.role === 'student')
-    //   .filter(s => s.deliverables.length >= 1)
-    //   .map(s => ({ student: s._id, deliverables: s.deliverables }));
-    // // console.log(students);
-    // // Also pass down student name.
-
-    // const flatSingle = arr => [].concat(...arr);
-
-    // const flattenedStudents = flatSingle(
-    //   students.map(eachFlatStudent =>
-    //     eachFlatStudent.deliverables.map(deliverable => ({
-    //       student: eachFlatStudent.student,
-    //       // tslint:disable-next-line
-    //       deliverable: deliverable,
-    //     })),
-    //   ),
-    // );
-    // const studentDeliverables = flattenedStudents.map(studentDeliverable => ({
-    //   student: studentDeliverable.student,
-    //   turnedIn: studentDeliverable.deliverable.turnedIn,
-    //   deliverableName: studentDeliverable.deliverable.name,
-    //   deliverableUrl: studentDeliverable.deliverable.deliverable,
-    //   deliverableId: studentDeliverable.deliverable._id,
-    // }));
-
-    // const turnedIn = studentDeliverables.filter(
-    //   d => d.turnedIn !== null && d.turnedIn !== undefined,
-    // );
-
-    // Intructor can mark as completed and with a grade.
-
-    // const toMarkDone = await userRepository.findOne(turnedIn[0].student);
-    // console.log(toMarkDone);
+    // // Instructor finds all deliverables marked turned in
+    // await gradeTurnedInDeliverable(deliverableRepository, manager);
   } catch (error) {
     console.log('Something went wrong', error);
   }
 });
+
+async function gradeTurnedInDeliverable(deliverableRepository, manager) {
+  const allDelivered = await deliverableRepository.find({
+    where: { turnedIn: { $ne: null } },
+  });
+  console.log(allDelivered[0]._id);
+  // Get ID of the one deliverable, which is [0] here
+  // This will come in from the form usually.
+  const deliveredID = allDelivered[0];
+  // Get the deliverable by ID and update the completed and grade fields
+  const acceptedAssignment = await manager.updateOne(Deliverable, deliveredID, {
+    $set: { completed: true, grade: '20' },
+  });
+  console.log(acceptedAssignment);
+  const accepted = await deliverableRepository.find({
+    where: { turnedIn: { $ne: null } },
+  });
+  console.log(accepted[0]);
+}
+
+async function handInAssignment(
+  userRepository,
+  deliverableRepository,
+  manager,
+) {
+  const someStudent = await userRepository.findOne({
+    where: { lastName: 'Hulbert' },
+  });
+  // Student pulls a particular deliverable from the list of deliverables
+  const someDeliverable = await deliverableRepository.findOne(
+    someStudent.deliverables[0],
+  );
+  console.log(someDeliverable);
+  //  Student turns in assignment
+  const savedAssignment = await manager.updateOne(
+    Deliverable,
+    someDeliverable,
+    {
+      $set: { turnedIn: new Date(), deliverable: 'http://www.google.com' },
+    },
+  );
+  console.log(savedAssignment);
+}
 
 async function assignmentToDeliverables() {
   const userRepository = getRepository(User);
@@ -100,12 +100,12 @@ async function assignmentToDeliverables() {
   const thisCohort = await cohortRepository.findOne({
     where: { name: 'WDI22' },
   });
-  thisCohort.students.forEach(async student => {
+  thisCohort.students.forEach(async studentId => {
     // For each id in cohort, pull a student
     // Create a new Deliverable, copying assignment into new deliverable
     const deliverable = new Deliverable();
     deliverable.name = thisAssignment.name;
-    deliverable.student.push(student);
+    deliverable.student.push(studentId);
     deliverable.cohort.push(thisCohort._id);
     deliverable.instructions = thisAssignment.instructions;
     deliverable.instructor = thisAssignment.instructor;
@@ -113,8 +113,18 @@ async function assignmentToDeliverables() {
     deliverable.topics = thisAssignment.topics;
     deliverable.deadline = new Date('2019-02-11');
 
-    const savedDeliverable = await manager.save(deliverable);
-    console.log(savedDeliverable);
+    const freshDeliverable = await manager.save(deliverable);
+    const savedDeliverable = await deliverableRepository.findOne(
+      freshDeliverable,
+    );
+
+    // Save deliverable to student
+    const student = await userRepository.findOne(studentId);
+
+    student.deliverables.push(savedDeliverable._id);
+    // console.log(student);
+    const savedStudent = await manager.save(student);
+    console.log(savedStudent);
 
     // Push deliverable
   });
